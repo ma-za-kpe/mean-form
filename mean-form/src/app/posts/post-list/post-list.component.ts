7ac6110c3cd17741ed03d714f23cd58bf856ffbf
ma-za-kpe <makpalyy@gmail.com>
2018-08-24 11:46:54 +0300

    add angular frontend

---
+++ mean-form/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from '../post.model';
+import { PostService } from '../posts.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+  // posts = [ this is hard coded data
+  //   {title: 'one', content: '1'},
+  //   {title: 'two', content: '2'},
+  //   {title: 'three', content: '3'},
+  //   {title: 'four', content: '4'}
+  // ];
+
+  posts: Post[] = [];
+  private postSub: Subscription;
+
+  constructor(public postService: PostService) {}
+
+  ngOnInit() {
+    this.posts = this.postService.getPosts();
+    this.postSub = this.postService.getPostsUpdateListener()
+    .subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOndestroy() {
+    this.postSub.unsubscribe();
+  }
+}
